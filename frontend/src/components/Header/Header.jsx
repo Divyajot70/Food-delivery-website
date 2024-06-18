cabac3ffa19d6eb870c4efbafcf037db5e195375
Divyajot Sapra <dsapra_be21@thapar.edu>
2024-06-18 22:20:08 +0530

    first commit

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-contents">
+            <h2>
+                Order your favourite food here
+            </h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, magnam fuga mollitia ut cumque molestias nemo repellat doloremque! Aliquam, accusamus nemo quibusdam adipisci eum sint fuga rem, beatae aperiam asperiores quae earum ea quos eos distinctio quam nam eveniet dolor ducimus exercitationem natus possimus temporibus soluta! In animi fugiat dolorum.</p>
+            <button>View Menu</button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
