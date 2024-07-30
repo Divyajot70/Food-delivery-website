3a510324b57557a1e28debb03ebd0145d8b8018f
Divyajot Sapra <dsapra_be21@thapar.edu>
2024-07-30 20:55:59 +0530

    commit 2

---
+++ frontend/src/pages/Verify/Verify.jsx
@@ -0,0 +1,20 @@
+import React, { useContext, useEffect } from 'react'
+import './Verify.css'
+import { useNavigate, useSearchParams } from 'react-router-dom'
+import { StoreContext } from '../../context/StoreContext';
+import axios from 'axios';
+const Verify = () => {
+
+    const [searchParams,setSearchParams] = useSearchParams();
+    const success = searchParams.get("success")
+    const orderId = searchParams.get("orderId")
+    const {url} = useContext(StoreContext);
+    
+  return (
+    <div className='verify'>
+      <div className="spinner"></div>
+    </div>
+  )
+}
+
+export default Verify
